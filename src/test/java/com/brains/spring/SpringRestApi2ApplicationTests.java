7f5b2104a54c82cac35da150f49e08f1c0ba5571
Sreee999 <edify99sree@gmail.com>
2025-05-27 21:00:45 +0530

    first commit

---
+++ src/test/java/com/brains/spring/SpringRestApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brains.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
